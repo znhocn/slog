152b7f1167a91f9f30d227da6138d2f942333a89
ZnHoCn <zenudnju@gmail.com>
2017-02-26 14:35:16 +0800

    Updated: 2017-02-26 14:35

--- slog.sh
+++ slog.sh
@@ -2,11 +2,11 @@
 
 cd $(cd `dirname $0`; pwd)
 touch date.log
-echo "$(date +%F) is OK" >> date.log
+echo "$(date +%F\ %H\:%M) is OK" >> date.log
 touch data.txt
 cat /dev/urandom | base64 | head -c 1000 > data.txt
 
 git pull
 git add .
-git commit -am "Updated: $(date +%F )"
+git commit -am "Updated: $(date +%F\ %H\:%M)"
 git push
