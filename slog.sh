68c8e564fc703a9de6e8d67d9205449155351ef9
ZnHoCn <me@hakr.xyz>
2018-09-03 06:00:10 +0800

    Updated: 2018-09-03 06:00

--- slog.sh
+++ slog.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+date_file="date_$(date +%Y).log"
+data_file="data.txt"
+
 cd $(cd `dirname $0`; pwd)
 git pull
 
-touch date.log
-echo "$(date +%F\ %H\:%M) is OK" >> date.log
-touch data.txt
-cat /dev/urandom | base64 | head -c 1000 > data.txt
+touch $date_file
+echo "$(date +%F\ %H\:%M) is OK" >> $date_file
+touch $data_file
+cat /dev/urandom | base64 | head -c 1000 > $data_file
 
 git add .
 git commit -am "Updated: $(date +%F\ %H\:%M)"
